9ae288ec3802679c8fac5dd80b42cba70ec3ed43
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2021-09-11 14:11:45 +0300

    add bindings to mimic the interface of the JS lib

---
+++ js-binding/index.js
@@ -0,0 +1,22 @@
+// Note that a dynamic `import` statement here is required due to
+// webpack/webpack#6615, but in theory `import { greet } from './pkg';`
+// will work here one day as well!
+const rust = import('./pkg');
+
+rust
+    .then(dpp => {
+        const { Identifier } = dpp;
+        const identifier = Identifier.fromString("EDCuAy8AXqAh56eFRkKRKb79SC35csP3W9VPe1UMaz87")
+        console.log(identifier.toString());
+        const buf = identifier.toBuffer();
+        console.log(identifier.toBuffer());
+        console.log(Array.from(identifier.toBuffer()).map(u8 => u8.toString(16)).join(''))
+        const id2 = new Identifier(identifier.toBuffer());
+        console.log('id2', id2.toString());
+        const id3 = Identifier.from("EDCuAy8AXqAh56eFRkKRKb79SC35csP3W9VPe1UMaz87")
+        console.log('id3', id3.toString());
+        console.log('buf:', buf);
+        const id4 = Identifier.from(buf);
+        console.log('id4', id4.toString());
+    })
+    .catch(console.error);
