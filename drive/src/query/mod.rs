62831c9727b4de2bcec1cedf1a39b133237d4e4f
Ivan Shumkov <ivan@shumkov.ru>
2022-02-10 18:59:29 +0700

    docs: add comment

--- drive/src/query/mod.rs
+++ drive/src/query/mod.rs
@@ -1164,6 +1164,9 @@ impl<'a> DriveQuery<'a> {
             })
             .collect();
 
+        // TODO: It's done in a terrable way due to unblock integration ASAP
+        //   must be refactoring in the upcoming PR
+
         let is_primary_key_query = fields.contains(String::from("$id"));
 
         let path_query = if !is_primary_key_query {
