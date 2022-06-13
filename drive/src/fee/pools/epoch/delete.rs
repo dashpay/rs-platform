f4c7377ad29a0a8b146b4580331cab691e56a7c9
Djavid Gabibiyan <djavidoff@gmail.com>
2022-06-13 17:59:37 +0500

    chore: delete epoch

--- drive/src/fee/pools/epoch/delete.rs
+++ drive/src/fee/pools/epoch/delete.rs
@@ -1,11 +1,14 @@
 use grovedb::TransactionArg;
 
-use crate::error::Error;
+use crate::{error::Error, fee::pools::fee_pools::FeePools};
 
 use super::epoch_pool::EpochPool;
 
 impl<'e> EpochPool<'e> {
     pub fn delete(&self, transaction: TransactionArg) -> Result<(), Error> {
-        Ok(())
+        self.drive
+            .grove
+            .delete(FeePools::get_path(), &self.key, transaction)
+            .map_err(Error::GroveDB)
     }
 }
