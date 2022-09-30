9232ed408a9e1331b04b2a326ffaa70c3b66ae0a
Pawel <rayven@null.net>
2022-09-30 07:30:57 +0200

    dry run in state transition execution context

--- dpp/src/state_transition/state_transition_execution_context.rs
+++ dpp/src/state_transition/state_transition_execution_context.rs
@@ -3,19 +3,48 @@ use super::fee::operations::Operation;
 #[derive(Debug, Clone, Default)]
 
 pub struct StateTransitionExecutionContext {
-    operations: Vec<Operation>,
+    actual_operations: Vec<Operation>,
+    dry_run_operations: Vec<Operation>,
+    is_dry_run: bool,
 }
 
 impl StateTransitionExecutionContext {
+    /// Add [`Operation`] into execution context
     pub fn add_operation(&mut self, operation: Operation) {
-        self.operations.push(operation);
+        if self.is_dry_run {
+            self.dry_run_operations.push(operation);
+        } else {
+            self.actual_operations.push(operation);
+        }
     }
 
+    /// Replace all existing operations with a new collection of operations
     pub fn set_operations(&mut self, operations: Vec<Operation>) {
-        self.operations = operations
+        self.actual_operations = operations
     }
 
-    pub fn get_operations(&self) -> &[Operation] {
-        &self.operations
+    /// Returns all (actual & dry run) operations
+    pub fn get_operations(&self) -> impl Iterator<Item = &Operation> {
+        self.actual_operations
+            .iter()
+            .chain(self.dry_run_operations.iter())
+    }
+
+    /// Enable dry run
+    pub fn enable_dry_run(&mut self) {
+        self.is_dry_run = true;
+    }
+
+    /// Disable dry run
+    pub fn disable_dry_run(&mut self) {
+        self.is_dry_run = false;
+    }
+
+    pub fn clear_dry_run_operations(&mut self) {
+        self.dry_run_operations.clear()
+    }
+
+    pub fn is_dry_run(&self) -> bool {
+        self.is_dry_run
     }
 }
