39673554264fa49fcfec545315caa29ee9ce1723
Djavid Gabibiyan <djavidoff@gmail.com>
2022-11-02 21:03:44 +0500

    chore: wip

--- dpp/src/identity/state_transition/identity_credit_withdrawal_transition/apply_identity_credit_withdrawal_transition_factory.rs
+++ dpp/src/identity/state_transition/identity_credit_withdrawal_transition/apply_identity_credit_withdrawal_transition_factory.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 use anyhow::{anyhow, Result};
 use chrono::Utc;
 use dashcore::{
@@ -12,21 +14,23 @@ use serde_json::{Map, Value as JsonValue};
 
 use crate::{
     data_contract::DataContract,
-    document::{generate_document_id::generate_document_id, Document},
+    document::Document,
     identity::convert_credits_to_satoshi,
     prelude::{Identifier, Identity},
     state_repository::StateRepositoryLike,
+    state_transition::StateTransitionConvert,
     util::entropy_generator::generate,
-    version::LATEST_VERSION,
 };
 
 use super::IdentityCreditWithdrawalTransition;
 
 const WITHDRAWAL_DATA_CONTRACT_ID_BYTES: [u8; 32] = [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    54, 98, 187, 97, 225, 127, 174, 62, 162, 148, 207, 96, 49, 151, 251, 10, 171, 109, 81, 24, 11,
+    216, 182, 16, 76, 73, 68, 166, 47, 226, 217, 127,
 ];
 const WITHDRAWAL_DATA_CONTRACT_OWNER_ID_BYTES: [u8; 32] = [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    170, 138, 235, 213, 173, 122, 202, 36, 243, 48, 61, 185, 146, 50, 146, 255, 194, 133, 221, 176,
+    188, 82, 144, 69, 234, 198, 106, 35, 245, 167, 46, 192,
 ];
 
 pub struct ApplyIdentityCreditWithdrawalTransition<SR>
@@ -116,14 +120,14 @@ where
 
         let document_data = JsonValue::Object(document_data_map);
 
+        let document_id_bytes: [u8; 32] = state_transition
+            .hash(true)?
+            .try_into()
+            .map_err(|_| anyhow!("Can't convert state transition hash to a document id"))?;
+
         let withdrawal_document = Document {
-            protocol_version: LATEST_VERSION,
-            id: generate_document_id(
-                &data_contract_id,
-                &data_contract_owner_id,
-                &document_type,
-                &document_entropy,
-            ),
+            protocol_version: state_transition.protocol_version,
+            id: Identifier::new(document_id_bytes),
             document_type,
             revision: 0,
             data_contract_id,
@@ -149,6 +153,10 @@ where
             maybe_existing_identity.ok_or_else(|| anyhow!("Identity not found"))?;
 
         existing_identity = existing_identity.reduce_balance(state_transition.amount);
+
+        let updated_identity_revision = existing_identity.get_revision() + 1;
+
+        existing_identity = existing_identity.set_revision(updated_identity_revision);
 
         // TODO: we need to be able to batch state repository operations
         self.state_repository
