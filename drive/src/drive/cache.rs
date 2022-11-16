a9f8f9e38abbf8e5d7b77cf349f53ebde071c049
Ivan Shumkov <ivan@shumkov.ru>
2022-11-17 00:27:10 +0300

    tests: add more tests for get method

--- drive/src/drive/cache.rs
+++ drive/src/drive/cache.rs
@@ -189,13 +189,12 @@ mod tests {
         }
 
         #[test]
-        fn test_get_from_global_cache_when_transaction_cache_does_not_have_contract() {
+        fn test_get_from_global_cache_when_transactional_cache_does_not_have_contract() {
             let drive = setup_drive(None);
             let transaction = drive.grove.start_transaction();
 
             let data_contract_cache = DataContractCache::new(10, 10);
 
-            // Create global contract
             let fetch_info_global = Arc::new(ContractFetchInfo::default());
 
             let contract_id = fetch_info_global.contract.id().to_buffer();
@@ -209,6 +208,79 @@ mod tests {
                 .expect("should be present");
 
             assert_eq!(fetch_info_from_cache, fetch_info_global)
+        }
+
+        #[test]
+        fn test_get_from_transactional_cache() {
+            let drive = setup_drive(None);
+            let transaction = drive.grove.start_transaction();
+
+            let mut data_contract_cache = DataContractCache::new(10, 10);
+
+            let fetch_info_transactional = Arc::new(ContractFetchInfo::default());
+
+            let contract_id = fetch_info_transactional.contract.id().to_buffer();
+
+            data_contract_cache
+                .transactional_cache
+                .insert(&transaction, Arc::clone(&fetch_info_transactional));
+
+            let fetch_info_from_cache = data_contract_cache
+                .get(contract_id, Some(&transaction))
+                .expect("should be present");
+
+            assert_eq!(fetch_info_from_cache, fetch_info_transactional)
+        }
+
+        #[test]
+        fn test_get_from_correct_transactional_cache() {
+            let drive = setup_drive(None);
+            let transaction1 = drive.grove.start_transaction();
+            let transaction2 = drive.grove.start_transaction();
+
+            let mut data_contract_cache = DataContractCache::new(10, 10);
+
+            // Create transactional contract 1
+            let fetch_info_transactional1 = Arc::new(ContractFetchInfo::default());
+
+            let contract_id = fetch_info_transactional1.contract.id().to_buffer();
+
+            data_contract_cache
+                .transactional_cache
+                .insert(&transaction1, Arc::clone(&fetch_info_transactional1));
+
+            // Create transactional contract 2
+            let mut fetch_info_transactional2 = ContractFetchInfo::default();
+
+            fetch_info_transactional2.contract.increment_version();
+
+            let fetch_info_transactional2 = Arc::new(fetch_info_transactional2);
+
+            data_contract_cache
+                .transactional_cache
+                .insert(&transaction2, Arc::clone(&fetch_info_transactional2));
+
+            // Get a contract for contract 1
+
+            let fetch_info_from_transaction1 = data_contract_cache
+                .get(contract_id, Some(&transaction1))
+                .expect("should be present");
+
+            assert_eq!(fetch_info_from_transaction1, fetch_info_transactional1);
+
+            // Get a contract for contract 2
+
+            let fetch_info_from_transaction2 = data_contract_cache
+                .get(contract_id, Some(&transaction2))
+                .expect("should be present");
+
+            assert_eq!(fetch_info_from_transaction2, fetch_info_transactional2);
+
+            // Get a contract without transaction
+
+            let fetch_info_from_global_cache = data_contract_cache.get(contract_id, None);
+
+            assert!(fetch_info_from_global_cache.is_none());
         }
     }
 }
